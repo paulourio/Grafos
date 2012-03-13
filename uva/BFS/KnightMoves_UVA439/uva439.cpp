3c3f14938920f1ed9566738f1a57b8ef8ffa8af1
Paulo Roberto Urio <paulourio@gmail.com>
2012-03-13 09:16:53 -0300

    UVA439 in C++ passed.
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

--- uva/BFS/KnightMoves_UVA439/uva439.cpp
+++ uva/BFS/KnightMoves_UVA439/uva439.cpp
@@ -26,7 +26,7 @@ typedef struct gnode {
 	int value, color, time;
 	list<int> adj;
 } gnode;
-typedef vector<gnode> Graph;
+typedef vector<gnode *> Graph;
 
 Graph graph;
 queue<int> Q;
@@ -36,16 +36,16 @@ static inline int dequeue() { int r = Q.front(); Q.pop(); return r; }
 int TOTAL_MOVES;
 #define found		(TOTAL_MOVES > 0)
 
-static gnode &get_graph_node(int value)
+static gnode *get_graph_node(int value)
 {
 	foreach(Graph, it, graph)
-		if (it->value == value)
+		if ((*it)->value == value)
 			return *it;
 	gnode *nnode = new gnode;
 	nnode->color = WHITE;
 	nnode->time = 0;
 	nnode->value = value;
-	graph.push_back(*nnode);
+	graph.push_back(nnode);
 	return graph.back();
 }
 
@@ -55,35 +55,34 @@ static void initialize_graph(int S)
 	while (!Q.empty())
 		Q.pop();
 	graph.clear();
-	get_graph_node(S).color = GRAY; /* Add node S into graph */
+	get_graph_node(S)->color = GRAY; /* Add node S into graph */
 }
 
-static void add_edge(gnode &from, int letter, int number)
+static void add_edge(gnode *from, int letter, int number)
 {
-	gnode& edge = get_graph_node(PACK(letter, number));
+	gnode *edge = get_graph_node(PACK(letter, number));
 
-	if (edge.color != WHITE)
+	if (edge->color != WHITE)
 		return;
-	edge.time = from.time + 1;
-	err("\tAdding %c%c (%d) -> %c%c (%d) -- time %d\n", UNPACK(from.value),
-			from.value, UNPACK(edge.value),
-			edge.value, edge.time);
-	from.adj.push_back(edge.value);
-	enqueue(edge.value);
-	if (edge.value == to)
-		TOTAL_MOVES = edge.time;
+	edge->time = from->time + 1;
+	err("\tAdding %c%c -> %c%c -- time %d\n", UNPACK(from->value),
+			UNPACK(edge->value), edge->time);
+	from->adj.push_back(edge->value);
+	enqueue(edge->value);
+	if (edge->value == to)
+		TOTAL_MOVES = edge->time;
 }
 
-#define letter_in_board(v)	('a' <= v && v < 'h')
-#define number_in_board(v)	('1' <= v && v < '8')
-static void generate_square_edges(gnode &n)
+#define letter_in_board(v)	('a' <= v && v <= 'h')
+#define number_in_board(v)	('1' <= v && v <= '8')
+static void generate_square_edges(gnode *n)
 {
 	int deltax[8] = {-2, -2, -1, -1, +1, +1, +2, +2};
 	int deltay[8] = {-1, +1, -2, +2, -2, +2, -1, +1};
 
 	for (int i = 0;  !found && i < 8;  i++) {
-		int X = LETTER(n.value) + deltax[i];
-		int Y = NUMBER(n.value) + deltay[i];
+		int X = LETTER(n->value) + deltax[i];
+		int Y = NUMBER(n->value) + deltay[i];
 		if (letter_in_board(X) && number_in_board(Y))
 			add_edge(n, X, Y);
 	}
@@ -91,11 +90,11 @@ static void generate_square_edges(gnode &n)
 
 static void BFS_next_edge(void)
 {
-	gnode& gcurr = get_graph_node(dequeue());
+	gnode *gcurr = get_graph_node(dequeue());
 
-	err("Working on %c%c (%d) time: %d\n", UNPACK(gcurr.value), gcurr.value, gcurr.time);
+	err("Working on %c%c time: %d\n", UNPACK(gcurr->value), gcurr->time);
 	generate_square_edges(gcurr);
-	gcurr.color = BLACK;
+	gcurr->color = BLACK;
 }
 
 static int BFS(void)
@@ -118,6 +117,7 @@ int main(void)
 		int total = (from != to?  BFS():  0);
 		printf("To get from %c%c to %c%c takes %u knight moves.\n",
 			UNPACK(from), UNPACK(to), total);
+		/* LOL: we are not free'ing anything at all :D */
 	}
 	return 0;
 }
