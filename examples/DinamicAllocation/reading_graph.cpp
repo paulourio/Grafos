e956306fd2fdc7a860b238bb3cbc771e8bdb71c8
Paulo Roberto Urio <paulourio@gmail.com>
2012-03-16 18:49:04 -0300

    Dinamic Allocation
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

---
+++ examples/DinamicAllocation/reading_graph.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <list>
+#include <map>
+
+using namespace std;
+
+struct gnode {
+	int value;
+	list<int> adj;
+};
+
+typedef map<int, gnode *> Graph;
+
+Graph graph;
+
+int main(void)
+{
+	int from, to;
+	while (scanf("%d %d", &from, &to) == 2) {
+		if (graph[from] == NULL)
+			graph[from] = new gnode;
+		graph[from]->adj.push_back(to);
+	}
+	/* Free'ing the graph.  Sometimes it is not necessary (like maraton) */
+	for (Graph::iterator it = graph.begin(); it != graph.end(); it++)
+		delete (*it).second;
+	return 0;
+}
